0b94062c3f8c0deabfc9cec3bec3d9547633e0ee
James Radtke <jradtke@redhat.com>
2016-05-17 16:37:00 -0400

    Update

--- app.rb
+++ app.rb
@@ -1,8 +1,29 @@
-require 'sinatra'
+require 'sinatra/base'
+require 'webrick'
+require 'webrick/https'
+require 'openssl'
 
 set :bind, '0.0.0.0'
 set :port, 8080
 
-get '/' do
-  "Sup?  I'm healthy!"
+CERT_PATH = '/opt/myCA/server/'
+
+webrick_options = {
+       :Port               => 8080,
+       :Logger             => WEBrick::Log::new($stderr, WEBrick::Log::DEBUG),
+       :DocumentRoot       => "/ruby/htdocs",
+       :SSLEnable          => true,
+       :SSLVerifyClient    => OpenSSL::SSL::VERIFY_NONE,
+       :SSLCertificate     => OpenSSL::X509::Certificate.new(  File.open(File.join(CERT_PATH, "healthcheck-https_stg_cloudapps_ao_dcn.crt")).read),
+       :SSLPrivateKey      => OpenSSL::PKey::RSA.new(          File.open(File.join(CERT_PATH, "healthcheck-https_stg_cloudapps_ao_dcn.key")).read),
+       :SSLCertName        => [ [ "CN",WEBrick::Utils::getservername ] ]
+}
+
+class MyServer  < Sinatra::Base
+   post '/' do
+     "Healthy Yo!!"
+   end            
 end
+
+Rack::Handler::WEBrick.run MyServer, webrick_options
+
